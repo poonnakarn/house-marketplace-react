c33ceb70485be399382ecf84ce2198631af76dcd
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-02-26 12:25:41 +0700

    Add react toastify

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -56,7 +57,8 @@ function SignUp() {
 
       navigate('/')
     } catch (error) {
-      console.log(error)
+      // console.log(error)
+      toast.error('Something went wrong with registration')
     }
   }
 
